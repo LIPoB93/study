36980e4525c70c54f3f6baf173cde8167e936c30
LIPoB93 <dft7577@gmail.com>
2026-06-04 14:15:31 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+{
+  "name": "전기산업기사 스터디 키",
+  "short_name": "스터디 키",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#101010",
+  "theme_color": "#171717",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
